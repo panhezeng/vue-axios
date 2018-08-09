381e38a97d346d497af093de5e9025e7438fedd4
panhezeng <panhezeng@gmail.com>
2018-08-09 10:00:32 +0800

    更新

--- example/components/App.vue
+++ example/components/App.vue
@@ -16,7 +16,7 @@
       }
     },
     created () {
-      this.$http.get('http://private-87040-publicexample.apiary-mock.com/upload/token').then((response) => {
+      this.$http.get('https://private-87040-publicexample.apiary-mock.com/upload/token').then((response) => {
         console.log(response)
         this.response = JSON.stringify(response)
       }).catch((error) => {

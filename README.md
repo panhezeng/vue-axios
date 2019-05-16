862581a9a21e32be851cc4fe082bec47761af1b6
panhezeng <panhezeng@gmail.com>
2019-05-16 16:14:49 +0800

    更新配置

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
 ```javascript
 import Vue from "vue";
 import VueAxios from "@panhezeng/vue-axios";
-Vue.use(VueAxios);
+Vue.use(VueAxios); ̰
 ```
 
 ### external vue 方式
@@ -84,5 +84,5 @@ npm run dev:example
 npm run build
 
 # 发版
-npm version patch && npm publish --access public
+npm set registry https://registry.npmjs.org/ && npm set @panhezeng:registry https://registry.npmjs.org/ && npm version patch && npm publish --access public && npm set registry https://registry.npm.taobao.org/ && npm set @panhezeng:registry https://registry.npm.taobao.org/
 ```
--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
-import axios from 'axios'
+import axios from "axios";
 
-let Vue
+let Vue;
 
-function install (_Vue) {
-
+function install(_Vue) {
   if (Vue) {
-    console.warn('[VueAxios] already installed. Vue.use(VueAxios) should be called only once.')
-    return
+    console.warn(
+      "[VueAxios] already installed. Vue.use(VueAxios) should be called only once."
+    );
+    return;
   }
 
-  Vue = _Vue
+  Vue = _Vue;
 
   Object.defineProperties(Vue.prototype, {
-    '$http': {
-      get: function () {
-        return axios
+    $http: {
+      get: function() {
+        return axios;
       }
     },
-    '$axios': {
-      get: function () {
-        return axios
+    $axios: {
+      get: function() {
+        return axios;
       }
     }
-  })
+  });
 
   // Vue添加全局属性
-  Vue.http = axios
-  Vue.axios = axios
+  Vue.http = axios;
+  Vue.axios = axios;
 }
 
 // auto install in dist mode
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
 }
 
 export const VueAxios = {
   install: install
-}
+};
